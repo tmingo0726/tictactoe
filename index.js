30ccaafa86ceea763d4cedae00e33db5d7bc7cc9
tmingo0726 <110070914+tmingo0726@users.noreply.github.com>
2022-08-17 14:37:20 -0500

    daily updates

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const state = {};
 
 const resetState = () => {
     state.board = ['','','','','','','','',''];
+    
 }
 
 const gameArea = document.getElementById('gameBoard');
 const boardEntries = document.getElementsByClassName('letterBox');
 const boardArr = [...boardEntries];
+const startButton = document.getElementById('startBtn');
 let playerOne = "";
 let playerTwo = "";
 let whoseTurn = "";
@@ -15,18 +17,19 @@ let whoseTurn = "";
 const savePlayer1 = () => {
     
     let player = document.getElementById("player1Value");
-    document.getElementById("player1Label").innerHTML = player.value + " " + "O";
+    let player2 = document.getElementById("player2Value");
     player.disabled="disabled";
     playerOne = document.getElementById("player1Value").value;
+    player2.disabled = false;
     whoseTurn = playerOne;
 }
 
 const savePlayer2 = () => {
     
     let player = document.getElementById("player2Value");
-    document.getElementById("player2Label").innerHTML = player.value + " " + "X";
     player.disabled="disabled";
     playerTwo = document.getElementById("player2Value").value;
+    startButton.disabled = false;
 }
 
 const resetGame = () => {
@@ -41,17 +44,62 @@ const resetGame = () => {
 
 const areSame = (val) => {
 
-    return val === 'X';
+    if (whoseTurn === playerOne)
+        return val === 'O';
+    else
+        return val === 'X';
 
 }
     
 const checkDiagonals = () => {
 
+    let tempArr = [];
+        
+    for (let i = 0; i < 9; i += 4) {
+        tempArr.push(boardArr[i].value);
+    }
+
+    if (tempArr.every(areSame))
+        return true;
+    
+    tempArr.length = 0;
+    for (let i = 2; i < 7; i += 2) {
+        tempArr.push(boardArr[i].value);
+    }
+    
+    if (tempArr.every(areSame))
+        return true;
+    
     return false;
 
 }
 
 const checkColumns = () => {
+
+    let tempArr = [];
+        
+    for (let i = 0; i < 7; i += 3) {
+        tempArr.push(boardArr[i].value);
+    }
+
+    if (tempArr.every(areSame))
+        return true;
+    
+    tempArr.length = 0;
+    for (let i = 4; i < 8; i += 3) {
+        tempArr.push(boardArr[i].value);
+    }
+    
+    if (tempArr.every(areSame))
+        return true;
+
+    tempArr.length = 0;
+    for (let i = 2; i < 9; i += 3) {
+        tempArr.push(boardArr[i].value);
+    }
+
+    if (tempArr.every(areSame))
+        return true;
 
     return false;
 
@@ -67,16 +115,16 @@ const checkRows = () => {
 
     if (tempArr.every(areSame))
         return true;
-    tempArr.length = 0;
 
+    tempArr.length = 0;
     for (let i = 3; i < 6; i++) {
         tempArr.push(boardArr[i].value);
     }
     
     if (tempArr.every(areSame))
         return true;
-    tempArr.length = 0;
 
+    tempArr.length = 0;
     for (let i = 6; i < 9; i++) {
         tempArr.push(boardArr[i].value);
     }
@@ -88,40 +136,49 @@ const checkRows = () => {
 
 }
 
+const renderCurrentPlayer = () => {
+
+    let text = `It is now ${whoseTurn}'s turn to play`;
+    document.getElementById('currentPlayerLabel').innerText = text;
+
+}
+
 const checkForWinner = () => {
 
     if (checkRows()) {
-        console.log("Rows: We have a winner");
+        console.log("Rows: We have a winner :", whoseTurn);
         return true;
     };
 
     if (checkColumns()) {
-        console.log("Columns We have a winner");
+        console.log("Columns We have a winner :", whoseTurn);
         return true;
     };
 
     if (checkDiagonals()){
-        console.log("Diagonals We have a winner");
+        console.log("Diagonals We have a winner :", whoseTurn);
         return true;
     };
 
     return false;
 }
+
+const startGame = () => {
+    renderCurrentPlayer();
+
+}
 gameArea.addEventListener('click', function(clickEvent) {
-    
-    //console.log("Inside event listener", whoseTurn);
     
     //Let's find out whose turn it is
     if (whoseTurn === playerOne) {
         clickEvent.target.value = 'O';
-        clickEvent.target.disabled = 'disabled';
-        whoseTurn = playerTwo;
     } else {
         clickEvent.target.value = 'X';
-        clickEvent.target.disabled = 'disabled';
-        whoseTurn = playerOne;
     }
    
     checkForWinner();
+    whoseTurn = whoseTurn === playerTwo ?  playerOne : playerTwo;
+    renderCurrentPlayer();
+        
 });
 
